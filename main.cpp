dcf2e245e701b405bcdd3d04868d9c996718b99d
acf-patrick <miharisoap@gmail.com>
2021-01-02 22:38:53 +0300

    system : avoid item removing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <ctime>
 #include <vector>
-#include <list>
 #include <iostream>
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
@@ -42,12 +41,6 @@ struct Vector
     Vector operator*(const float f)
     { return Vector(x*f, y*f); }
 };
-
-std::ostream& operator<<(std::ostream& stream, const Vector& v)
-{
-    stream << '(' << v.x << ", " << v.y << ')';
-    return stream;
-}
 
 class Dash
 {
@@ -99,7 +92,7 @@ private:
 		{}
 		void update() override
 		{
-            // v_magnitude -= 0.0001;
+            //v_magnitude -= 0.0001;
 			Dash::update();
 		}
 	};
@@ -107,20 +100,21 @@ private:
 	std::vector<Dash*> m_dash;
 	Vector start;
 	int distance;
+	bool spreading;
 
 public:
 	Spread(int x, int y) :
-		start(x, y), distance(randint(100, 150))
+		start(x, y), distance(randint(50, 100)),
+		spreading(true)
     {
     	int n = randint(50, 100);
     	float section = 2*pi/n;
+		Uint32 color = 0;
+		for (int k=0; k<6; ++k)
+			color += randint(6, 15)*std::pow(16, k);
         for (int i=0; i<n; ++i)
-		{
-			Uint32 color = 0;
-			for (int k=0; k<6; ++k)
-				color += (rand()%16)*std::pow(16, k);
-			m_dash.push_back(new dash(x, y, i*section, color));
-		}
+			if (rand()%2)
+				m_dash.push_back(new dash(x, y, i*section, color));
     }
 
     ~Spread()
@@ -132,17 +126,24 @@ public:
 
 	void draw(SDL_Surface* screen)
 	{
-		for (auto& d : m_dash)
-			d->draw(screen);
+		if (spreading)
+			for (auto& d : m_dash)
+				d->draw(screen);
 	}
 
 	bool update()
 	{
+		if (!spreading)
+			return false;
+
 		if (!m_dash.empty())
 		{
             Vector cur = m_dash[0]->getPosition();
             if ((cur - start).magnitude() >= distance)
+			{
+				spreading = false;
 				return false;
+			}
 		}
 
 		for (auto& d : m_dash)
@@ -155,56 +156,71 @@ public:
 class System
 {
 private:
-    Spread *spread;
+    std::vector<Spread*> spread;
     Dash *dash;
     Vector dest;
     class m_Dash : public Dash
     {
 	public:
 		m_Dash(Vector dest) :
-			Dash(width/2, height, 0, 0xffffff)
+			Dash(randint(width*0.25, width*0.75), height, 0, 0xffffff)
 		{
-			v_magnitude = 8;
+			v_magnitude = 5;
             velocity = dest - start;
             velocity.normalize();
+		}
+		void update() override
+		{
+            v_magnitude += 0.3;
+            Dash::update();
 		}
     };
 
 public:
-	System() : spread(nullptr), dest(randint(width/4, 0.75*width), randint(0, height/2))
+	System() : dest(randint(width/4, 0.75*width), randint(0, height/4))
 	{
         dash = new m_Dash(dest);
 	}
 
 	~System()
 	{
-		delete spread;
+		for (auto& s : spread)
+			delete s;
 		delete dash;
 	}
 
 	bool update()
     {
-    	if (spread)
+    	if (spread.empty())
 		{
-			if (!spread->update())
-				return false;
+            Vector position(dash->getPosition());
+            if ((dest - position).magnitude() <= 10)
+			{
+				spread.push_back(new Spread(dest.x, dest.y));
+				for (int i=0; i<2; ++i)
+					for (int j=0; j<2; ++j)
+						spread.push_back(new Spread(dest.x+(i?-5:5), dest.y+(j?-5:5)));
+			}
+			dash->update();
 		}
 		else
 		{
-            Vector position(dash->getPosition());
-            if ((dest - position).magnitude() <= 4)
-				spread = new Spread(dest.x, dest.y);
-			dash->update();
+			bool result(false);
+			for (auto& s : spread)
+				result += s->update();
+			if (!result)
+				return false;
 		}
 		return true;
     }
 
     void draw(SDL_Surface* screen)
     {
-    	if (spread)
-			spread->draw(screen);
-		else
+    	if (spread.empty())
 			dash->draw(screen);
+		else
+			for (auto& s : spread)
+				s->draw(screen);
     }
 };
 
